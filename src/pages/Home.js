598659dc3689b414ef0cc31a10d792856da61777
AdiFischer <adifischer1@gmail.com>
2022-11-21 14:07:03 -0500

    Header Footer AddPost

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,39 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function Home() {
+    const[posts, setPosts] = useState([])
 
     useEffect(() => {
         fetch('http://localhost:3030/')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setPosts(data))
         .catch(err => console.error(err))
     }, [])
+
+    console.log(posts)
+
+    const AllPosts = () => {
+        return(
+            <div className="post">
+                { posts.map((item, index) => {
+                return(
+                    <div className="post-item" key={item._id}>
+                    <img src={`https://source.unsplash.com/random?sig=${index}`} alt='' />
+                    <h4>{item.author}</h4>
+                    <p>{item.text}</p>
+                    <p>{item.date}</p>
+                    </div>
+                )
+            })} 
+            </div>
+            
+        )
+    }
 
     return(
         <div className="container">
             <h1>Home</h1>
-            <div className="post">
-            allposts here
-            </div>
+            <AllPosts />
         </div>
     )
 }
